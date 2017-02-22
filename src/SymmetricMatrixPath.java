a6a691b469546162bd0cc07053288a3a1d2b199d
now-raymond <rayjta@gmail.com>
2017-02-22 20:04:09 +0000

    Pathfinder parser

---
+++ src/SymmetricMatrixPath.java
@@ -0,0 +1,62 @@
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by Raymond on 22/2/2017.
+ */
+
+/**
+ * Holds paths. Indexed by ArrayList<Point2D> = matrix[a][b], which is a list of waypoints
+ */
+public class SymmetricMatrixPath {
+
+    // A triangular 3D matrix to hold the data.
+    // 1: Robot A
+    // 2: Robot B
+    // 3: ArrayList<Point2D> (This is the List of waypoints, in sequence)
+    private ArrayList<ArrayList<ArrayList<Point2D>>> matrix;
+
+    /**
+     * Creates an n-by-n symmetric matrix with an empty path.
+     */
+    public SymmetricMatrixPath(int n) {
+        matrix = new ArrayList<>(n);
+        for (int i=0; i<n; i++) {
+            matrix.add(i, new ArrayList<>(n));
+            /*ArrayList<ArrayList<Point2D>> inner = matrix.get(i);
+            inner = new ArrayList<>(n);*/
+
+            for (int j=0; j<n; j++) {
+                matrix.get(i).add(new ArrayList<>());
+                /*ArrayList<Point2D> pathArray = inner.get(j);
+                pathArray = new ArrayList<>(n);*/
+            }
+        }
+    }
+
+    /**
+     * Returns the path at position (row,col).
+     */
+    public ArrayList<Point2D> get( int row, int col ) {
+        return matrix.get(row).get(col);
+    }
+
+    /**
+     * Returns the path at position (row,col).
+     */
+    public ArrayList<Point2D> getPath( int row, int col ) {
+        ArrayList<Point2D> path;
+
+        if (row < col) {
+            path = matrix.get(row).get(col);
+            return path;
+        } else {
+            path = matrix.get(col).get(row);
+            // Shallow clone array and reverse the path.
+            ArrayList<Point2D> reversePath = (ArrayList<Point2D>) path.clone();
+            Collections.reverse(reversePath);
+            return reversePath;
+        }
+    }
+}
