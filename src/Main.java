efe06f0e5bfa81348b3383b3223e17ccc2e9f5e9
now-raymond <rayjta@gmail.com>
2017-02-22 21:45:25 +0000

    Update Main class to streamline structure

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class Main {
@@ -7,42 +6,28 @@ public class Main {
     final static String PASSWORD = "usrdoupjm8q94i7td9qctue39k";
     final static String INPUT_FILE = "robots.mat.txt";
 
-    private static String removeTrailingZeros(double d) {
-        return String.valueOf(d).replaceAll("[0]*$", "").replaceAll("\\.$", "");
-    }
+    // Fill in the problem number to attempt here.
+    final static int INSTANCE_NUMBER = 1;
 
     public static void main(String[] args) {
-
-        /*
-        * Trying it out
-        * */
         ArrayList<Instance> instances = new Instantiator().getInstances(INPUT_FILE);
-        Instance testInstance = instances.get(2);
-        ArrayList<Robot> robots = testInstance.getRobots();
-        System.out.println("--------------------Robot Coordinates--------------------");
-        for (Robot r: robots) {
-            System.out.print(r.getPosition().getX());
-            System.out.print(" ");
-            System.out.print(r.getPosition().getY());
-            System.out.println();
-        }
-        if ((testInstance.getObstacles()).isEmpty()) {
-            System.out.println("No obstacles in instance");
-        } else {
-            ArrayList<Obstacle> obstacles = testInstance.getObstacles();
-            System.out.println("--------------------Obstacle Coordinates--------------------");
-            for (int i = 0; i < obstacles.size(); i++) {
-                ArrayList<Point2D> coors = obstacles.get(i).getCoordinates();
-                System.out.println("--------------------Obstacle " + String.valueOf(i+1) + "--------------------");
-                for (Point2D c: coors) {
-                    System.out.print(c.getX());
-                    System.out.print(" ");
-                    System.out.print(c.getY());
-                    System.out.println();
-                }
 
-            }
-        }
+        Instance instance = instances.get(INSTANCE_NUMBER - 1);
 
+        ArrayList<Robot> robots = instance.getRobots();
+        ArrayList<Obstacle> obstacles = instance.getObstacles();
+
+        // Create PathFinder
+        PathFinder pf = new PathFinder(INSTANCE_NUMBER, robots, obstacles);
+
+        // Wake up the first robot.
+        instance.getRobots().get(0).status = Robot.Status.AWAKE;
+
+        // Do algorithm
+        // TODO - Place the call to do the algorithm here!
+
+        // --- Do other debug things here ---
+        // Generate visibility graph (comment out unless required)
+        //VisGraphGenerator.generate(instance);
     }
 }
