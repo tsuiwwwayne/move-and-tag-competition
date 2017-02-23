53c4b41d8038bcca55686566294afa66e3af3b86
now-raymond <rayjta@gmail.com>
2017-02-23 02:28:33 +0000

    Show status on visgen

--- py/loadgraph.py
+++ py/loadgraph.py
@@ -27,40 +27,40 @@ if (len(sys.argv) > 1):
     loopCounter = 0
     totalCount = len(robots)
     for pair in itertools.combinations(robots, 2):
-        #print("From: %d to %d:" % (pair[0].polygon_id, pair[1].polygon_id))
+        #print("From: %d to %d:" % (pair[0].extradata_1, pair[1].extradata_1))
         shortest = g.shortest_path(pair[0], pair[1])
         numWaypoints = len(shortest) - 2
         #print("Waypoint length: %d" % numWaypoints)
         
         if numWaypoints > 0:
             #print(shortest)
-            builtstr = "%d,%d#" % (pair[0].polygon_id, pair[1].polygon_id)
+            builtstr = "%d,%d#" % (pair[0].extradata_1, pair[1].extradata_1)
             # First element
-            builtstr += str(shortest[1].polygon_id)
+            builtstr += str(shortest[1].extradata_1)
             builtstr += ":"
-            builtstr += str(shortest[1].polygon_id_2)
+            builtstr += str(shortest[1].extradata_2)
             
             # All the rest
             for waypoint in shortest[2:-1]:
                 builtstr += "|"
-                builtstr += str(waypoint.polygon_id)
+                builtstr += str(waypoint.extradata_1)
                 builtstr += ":"
-                builtstr += str(waypoint.polygon_id_2)
+                builtstr += str(waypoint.extradata_2)
                 
             print(builtstr, file=f)
         
         loopCounter += 1
         if (loopCounter % 20 == 0):
             elapsed = time.clock() - t1
-            completed = pair[0].polygon_id
-            left = totalCount - pair[0].polygon_id
+            completed = pair[0].extradata_1
+            left = totalCount - pair[0].extradata_1
             if completed > 0:
                 time_for_one = elapsed / completed
             else:
                 time_for_one = 0
                 
             time_remaining = time_for_one * left
-            print("Status: %d:%d / %d. Elapsed: %f seconds. Estimated time remaining: %f s." % (completed, pair[1].polygon_id, totalCount, elapsed, time_remaining))
+            print("Status: %d:%d / %d. Elapsed: %f seconds. Estimated time remaining: %f s." % (completed, pair[1].extradata_1, totalCount, elapsed, time_remaining))
 
     print("Computing paths took %f seconds." % (time.clock()-t1))
     
